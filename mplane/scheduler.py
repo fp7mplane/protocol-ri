cbedbde0d95cba407877b1bd9dac823adf939cab
Brian Trammell <trammell@tik.ee.ethz.ch>
2014-01-28 17:30:05 +0100

    these are called services and jobs according to the current sketch

--- mplane/scheduler.py
+++ mplane/scheduler.py
@@ -25,14 +25,41 @@ results within the mPlane reference component.
 
 """
 
+class Service(object):
+	"""
+	A Service is a binding of some runnable code to an 
+	mplane.model.Capability provided by a component.
+
+	To use services with an mPlane scheduler, inherit from 
+	mplane.scheduler.Service or one of its subclasses 
+	and implement run().
+
+	"""
+	def __init__(self, capability):
+		super(Service, self).__init__()
+		self.capability = capability
+
+	def run(self, spec):
+		"""
+		Run this service given a specification which matches the capability.
+		This is called by the scheduler, and should be implemented by
+		a concrete subclass of Service.
+
+		"""
+		raise NotImplementedError("Cannot instantiate an abstract Service")
+
 class Job(object):
 	"""
-	Represents a measurement that can run, or is running, at a component.
+	A Job is a binding of some running code to an
+	mPlane.model.Specification within a component. A Job can
+	be thought of as a specific instance of a service presently
+	running at a 
 
 	"""
-	def __init__(self):
+	def __init__(self, service, specification):
 		super(Job, self).__init__()
-	
-	def run(self, params):
-		pass
-
+		self.service = service
+		self.specification = specification
+		
+		
+		
