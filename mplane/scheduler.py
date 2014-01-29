cee92c47551e45d23b28f1565f8f853f38b667d1
Brian Trammell <trammell@tik.ee.ethz.ch>
2014-01-29 19:37:47 +0100

    incremental (nonbuilding) scheduler progress

--- mplane/scheduler.py
+++ mplane/scheduler.py
@@ -39,11 +39,22 @@ class Service(object):
 		super(Service, self).__init__()
 		self.capability = capability
 
-	def run(self, spec):
+	def run(self, specification, check_interrupt):
 		"""
 		Run this service given a specification which matches the capability.
 		This is called by the scheduler, and should be implemented by
 		a concrete subclass of Service.
+
+		The implementation should extract its parameters from a given
+		mplane.model.Specification, and return its result values in a 
+		mplane.model.Result derived therefrom.
+
+		After each row or logically grouped set of rows, the implementation
+		should call the check_interrupt function to determine whether it 
+		should stop; if this function returns True, the implementation should 
+		terminate its processing in an orderly fashion and return its results.
+
+		Each method will be called within its own thread and/or process. 
 
 		"""
 		raise NotImplementedError("Cannot instantiate an abstract Service")
@@ -52,14 +63,63 @@ class Job(object):
 	"""
 	A Job is a binding of some running code to an
 	mPlane.model.Specification within a component. A Job can
-	be thought of as a specific instance of a service presently
-	running at a 
+	be thought of as a specific instance of a Service presently
+	running, or ready to run at some point in the future.
 
 	"""
 	def __init__(self, service, specification):
 		super(Job, self).__init__()
 		self.service = service
 		self.specification = specification
-		
-		
-		
+		self.result = None
+		self._thread = None
+		self._running = False
+		self._started_at = None
+		self._ended_at = None
+		self._interrupt = threading.Event()
+
+	def _run(self):
+		self._running = True
+		self.result = self.service.run(self.specification, 
+									   self._check_interrupt)
+		self._running = False
+
+	def _check_interrupt(self):
+		return self._interrupt.is_set()
+
+	def _schedule_now(self):
+		pass
+
+	def schedule(self):
+		"""
+		Schedule this job to run.
+
+ 		"""
+ 		start = self.specification.get_parameter_value("start")
+ 		end = self.specification.get_parameter_value("end")
+
+ 		# calculate a delay
+ 		if start.isinstance(datetime):
+			delay = start - datetime.utcnow()
+			if end.isinstance(datetime):
+				duration = end - start
+		elif ((start is mplane.model.time_now) or 
+	     	  (start is mplane.model.time_whenever):
+	     	# treat now and whenever as immediate start
+	     	# revisit this if we actually want to do prioritization
+			delay = timedelta()
+			if end.isinstance(datetime):
+				duration = end - start
+
+ 		if delay.total_seconds() > 0:
+ 			threading.Timer(delay.total_seconds(), self._schedule_now)
+
+ 		else:
+ 			self._schedule_now()
+
+	def interrupt(self):
+		"""
+		Interrupt this job.
+
+		"""
+		self._interrupt.set()
