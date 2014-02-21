95e9a4d5e9912394c8745f530a8a576d594bd4e7
Brian Trammell <trammell@tik.ee.ethz.ch>
2014-02-21 15:45:47 +0100

    when parse-unparse ready to test

--- mplane/tscope.py
+++ mplane/tscope.py
@@ -28,25 +28,91 @@ valid. The "schedule" section defines, for specifications, when a specified
 measurement should be repeated.
 
 This file holds the classes implementing this during development and experimentation;
-this code will be integrated into model.py
+this code will be integrated into model.py.
 
 """
 
-_iso8601_re = re.compile('(\d+-\d+-\d+)(\s+\d+:\d+(:\d+)?)?(\.\d+)?')
+TIME_PAST = "past"
+TIME_NOW = "now"
+TIME_FUTURE = "future"
+
+_iso8601_pat = '(\d+-\d+-\d+)(\s+\d+:\d+(:\d+)?)?(\.\d+)?'
+_iso8601_re = re.compile(_iso8601_pat)
 _iso8601_fmt = { 'us': '%Y-%m-%d %H:%M:%S.%f',
                   's': '%Y-%m-%d %H:%M:%S',
                   'm': '%Y-%m-%d %H:%M',
                   'd': '%Y-%m-%d'}
 
-_dur_re = re.compile('((\d+)d)((\d+)h)((\d+)m)((\d+)s)')
+_dur_pat = '((\d+)d)((\d+)h)((\d+)m)((\d+)s)'
+_dur_re = re.compile(_dur_pat)
 _dur_seclabel = ( (86400, 'd'),
                   ( 3600, 'h'),
                   (   60, 'm'),
                   (    1, 's') )
 
-def parse_8601(valstr):
-    if valstr is None or valstr == VALUE_NONE:
+_dow_label = ('so', 'mo', 'tu', 'we', 'th', 'fr', 'sa')
+
+class PastTime:
+    """
+    Class representing the indeterminate past. 
+    Do not instantiate; use the time_past instance of this class.
+
+    """
+    def __str__(self):
+        return TIME_PAST
+
+    def __repr__(self):
+        return "mplane.tscope.time_past"
+
+    def strftime(self, ign):
+        return str(self)
+
+
+time_past = PastTime()
+
+class NowTime:
+    """
+    Class representing the present.
+    Do not instantiate; use the time_now instance of this class.
+    
+    """
+    def __str__(self):
+        return TIME_NOW
+
+    def __repr__(self):
+        return "mplane.tscope.time_now"
+
+    def strftime(self, ign):
+        return str(self)
+
+time_now = NowTime()
+
+class FutureTime:
+    """
+    Class representing the indeterminate future.
+    Do not instantiate; use the time_future instance of this class.
+
+    """
+    def __str__(self):
+        return TIME_FUTURE
+
+    def __repr__(self):
+        return "mplane.tscope.time_future"
+
+    def strftime(self, ign):
+        return str(self)
+
+time_future = FutureTime()
+
+def parse_time(valstr):
+    if valstr is None:
         return None
+    elif valstr == TIME_PAST:
+        return time_past
+    elif valstr == TIME_FUTURE:
+        return time_future
+    elif valstr == TIME_NOW:
+        return time_now
     else:
         mg = _iso8601_re.match(valstr).groups()
         if mg[2]:
@@ -60,7 +126,7 @@ def parse_8601(valstr):
             dt = datetime.strptime(valstr, "%Y-%m-%d")
         return dt
     
-def unparse_8601(valts, precision="us"):
+def unparse_time(valts, precision="us"):    
     return valts.strftime(_iso8601_fmt[precision])
 
 def parse_dur(valstr):
@@ -89,14 +155,55 @@ def unparse_dur(valtd):
 class When(object):
     """
     Defines the temporal scopes for capabilities, results, or 
-    single measurement specifications in terms of two specific 
-    points in time.
+    single measurement specifications.
 
     """
-    def __init__(self):
+    def __init__(self, valstr=None, a=None, b=None, d=None, p=None):
         super(When, self).__init__()
-        self._a = None
-        self._b = None
+        self._a = a
+        self._b = b
+        self._d = d
+        self._p = p
+
+        if valstr is not None:
+            self._parse(valstr)
+        
+    def _parse(self, valstr):
+        # First separate the period from the value and parse it
+        valsplit = valstr.split(PERIOD_SEP)
+        if len(valsplit) > 1:
+            (valstr, perstr) = valsplit
+            self._p = parse_dur(perstr)
+        else:
+            self._p = None
+
+        # then try to split duration or range
+        valsplit = valstr.split(DURATION_SEP)
+        if len(valsplit) > 1:
+            (valstr, durstr) = valsplit
+            self._d = parse_dur(durstr)
+            valsplit = [valstr]
+        else:
+            self._d = None
+            valsplit = valstr.split(RANGE_SEP)
+        
+        self._a = parse_time(valsplit[0])
+        if len(valsplit) > 1:
+            self._b = parse_time(valsplit[1])
+        else:
+            self._b = None
+
+    def __str__(self):
+        valstr = unparse_time(self._a)
+
+        if self._b is not None:
+            valstr = " ".join(valstr, RANGE_SEP, unparse_time(self._b))
+        elif self._d is not None:
+            valstr = " ".join(valstr, DURATION_SEP, unparse_dur(self._d))
+
+        if (self._p) is not None:
+            valstr = " ".join(valstr, PERIOD_SEP, unparse_dur(self._p))
+
 
 class Schedule(object):
     """
