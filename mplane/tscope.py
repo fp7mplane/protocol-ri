2bd398003d9eb55030915eb01e77452da396c6fc
Brian Trammell <brian@trammell.ch>
2014-02-21 11:29:16 +0100

    moving toward lmap compatibility

--- mplane/tscope.py
+++ mplane/tscope.py
@@ -32,17 +32,23 @@ this code will be integrated into model.py
 
 """
 
-_iso8601_re = re.compile('(\d+-\d+-\d+)(\s+\d+:\d+(:\d+)?)?(\n\d+)?')
+_iso8601_re = re.compile('(\d+-\d+-\d+)(\s+\d+:\d+(:\d+)?)?(\.\d+)?')
 _iso8601_fmt = { 'us': '%Y-%m-%d %H:%M:%S.%f',
                   's': '%Y-%m-%d %H:%M:%S',
                   'm': '%Y-%m-%d %H:%M',
                   'd': '%Y-%m-%d'}
 
+_dur_re = re.compile('((\d+)d)((\d+)h)((\d+)m)((\d+)s)')
+_dur_seclabel = ( (86400, 'd'),
+                  ( 3600, 'h'),
+                  (   60, 'm'),
+                  (    1, 's') )
+
 def parse_8601(valstr):
     if valstr is None or valstr == VALUE_NONE:
         return None
     else:
-        mg = _timestamp_re.match(valstr).groups()
+        mg = _iso8601_re.match(valstr).groups()
         if mg[2]:
             dt = datetime.strptime(valstr, "%Y-%m-%d %H:%M:%S")
             if mg[3]:
@@ -58,10 +64,27 @@ def unparse_8601(valts, precision="us"):
     return valts.strftime(_iso8601_fmt[precision])
 
 def parse_dur(valstr):
-    pass
+    if valstr is None or valstr == VALUE_NONE:
+        return None
+    else:
+        mh = _dur_re.match(valstr),groups()
+        valsec = 0
+        for i in range(3):
+            if mg[2*i + 1]:
+                valsec += _dur_seclabel[i][0] * int(mg[2*i + 1])
+    return timedelta(seconds=valsec)
 
 def unparse_dur(valtd):
-    pass
+    valsec = int(valtd.total_seconds):
+    valstr = ""
+    for i in range(3):
+        if valsec > _dur_seclabel[i][0]:
+            valunit = int(valsec / _dur_seclabel[i][0])
+            valstr += str(valunit) + _dur_seclabel[i][1]
+            valsec -= valunit * _dur_seclabel[i][0]
+    if len(valstr) == 0:
+        valstr = "0s"
+    return valstr
 
 class When(object):
     """
@@ -72,3 +95,20 @@ class When(object):
     """
     def __init__(self):
         super(When, self).__init__()
+        self._a = None
+        self._b = None
+
+class Schedule(object):
+    """
+    Defines a schedule for repeated operations based on crontab-like
+    sets of months, days, days of weeks, hours, minutes, and seconds.
+    Used to specify repetitions 
+    """
+    def __init__(self):
+        super(Schedule, self).__init__()
+        self._months = set()
+        self._days = set()
+        self._weekdays = set()
+        self._hours = set()
+        self._minutes = set()
+        self._seconds = set()
