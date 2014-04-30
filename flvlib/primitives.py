3b6f98ed7bc7cfbac7bf620577fb728c464bdfa8
szemtiv <tivadar.szemethy@netvisor.hu>
2014-04-30 15:49:08 +0200

    added the extended version of flvlib we use for FLV parsing (forked from https://github.com/wulczer/flvlib)

---
+++ flvlib/primitives.py
@@ -0,0 +1,108 @@
+import struct
+
+"""
+The internal FLV representations of numbers.
+"""
+
+
+__all__ = ['get_ui32', 'make_ui32', 'get_si32_extended', 'make_si32_extended',
+           'get_ui24', 'make_ui24', 'get_ui16', 'make_ui16',
+           'get_si16', 'make_si16', 'get_ui8', 'make_ui8',
+           'get_double', 'make_double', 'EndOfFile']
+
+
+class EndOfFile(Exception):
+    pass
+
+
+# UI32
+def get_ui32(f):
+    try:
+        ret = struct.unpack(">I", f.read(4))[0]
+    except struct.error:
+        raise EndOfFile
+    return ret
+
+def make_ui32(num):
+    return struct.pack(">I", num)
+
+
+# SI32 extended
+def get_si32_extended(f):
+    # The last 8 bits are the high 8 bits of the whole number
+    # That's how Adobe likes it. Go figure...
+    low_high = f.read(4)
+    if len(low_high) < 4:
+        raise EndOfFile
+    combined = struct.pack("BBBB", low_high[3], low_high[0], low_high[1], low_high[2])
+    return struct.unpack(">i", combined)[0]
+
+def make_si32_extended(num):
+    ret = struct.pack(">i", num)
+    # ret =  ret[1:] + bytes(ret[0])
+    ret = struct.pack("BBBB", ret[1], ret[2], ret[3], ret[0])
+    return ret
+
+
+# UI24
+def get_ui24(f):
+    try:
+        high, low = struct.unpack(">BH", f.read(3))
+    except struct.error:
+        raise EndOfFile
+    ret = (high << 16) + low
+    return ret
+
+def make_ui24(num):
+    ret = struct.pack(">I", num)
+    return ret[1:]
+
+
+# UI16
+def get_ui16(f):
+    try:
+        ret = struct.unpack(">H", f.read(2))[0]
+    except struct.error:
+        raise EndOfFile
+    return ret
+
+def make_ui16(num):
+    return struct.pack(">H", num)
+
+
+# SI16
+def get_si16(f):
+    try:
+        ret = struct.unpack(">h", f.read(2))[0]
+    except struct.error:
+        raise EndOfFile
+    return ret
+
+def make_si16(num):
+    return struct.pack(">h", num)
+
+
+# UI8
+def get_ui8(f):
+    try:
+        ret = struct.unpack("B", f.read(1))[0]
+    except struct.error:
+        raise EndOfFile
+    return ret
+
+def make_ui8(num):
+    return struct.pack("B", num)
+
+
+
+# DOUBLE
+def get_double(f):
+    data = f.read(8)
+    try:
+        ret = struct.unpack(">d", data)[0]
+    except struct.error:
+        raise EndOfFile
+    return ret
+
+def make_double(num):
+    return struct.pack(">d", num)
