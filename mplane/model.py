b3f07f3220ade0ac6543be571b1d46cd8a7da4b2
Stefano Pentassuglia <stefano.pentassuglia@ssbprogetti.it>
2014-12-09 18:01:22 +0100

    minor bugfix in model.py

--- mplane/model.py
+++ mplane/model.py
@@ -1862,7 +1862,7 @@ class Metavalue(Element):
                " value "+repr(self._val)+" >"
 
     def set_value(self, val):
-        if instanceof(val, str):
+        if isinstance(val, str):
             val = self._prim.parse(val)
         self._val = val
 
