11fa8084313f77e6dcd4d53867d286ec30f88694
root <root@h139-40.rackhostvps.com>
2015-05-08 15:33:05 +0200

    - fixing urllib3 warning - command line parameters

--- mplane/ott.py
+++ mplane/ott.py
@@ -25,6 +25,9 @@
 Implements OTT probe for integration into 
 the mPlane reference implementation.
 
+TODO:
+ * currently one count is supported
+
 """
 
 import argparse
@@ -50,6 +53,9 @@ import mplane.scheduler
 import socket
 import sys
 
+_ottopt_period = "--mplane"
+_ottopt_url    = "--url"
+
 DEFAULT_SUPERVISOR_IP4  = 'localhost'
 DEFAULT_SUPERVISOR_PORT = 8888
 
@@ -57,146 +63,112 @@ REGISTRATION_PATH  = "register/capability"
 SPECIFICATION_PATH = "show/specification"
 RESULT_PATH        = "register/result"
 
-urllib3.disable_warnings()
+caplist = ["bandwidth.nominal.kbps", "http.code.max", "http.redirectcount.max", "qos.manifest", "qos.content", "qos.aggregate", "qos.level"]
+#urllib3.disable_warnings()
 
+# this method is for starting the OTT measurement. probe-ott will return with a measurement JSON.
+def _ott_process(period=None, url=None):
+    ott_argv = ["probe-ott"]
+    ott_argv += [ "--slot", "-1"]
+    if period is not None:
+        ott_argv += [_ottopt_period, str(int(period))]
+    if url is not None:
+        ott_argv += [_ottopt_url, str(url)]
 
+    print("running " + " ".join(ott_argv))
+
+    return subprocess.Popen(ott_argv, stdout=subprocess.PIPE)
+
+# assemble offered parameters
+def capability(ipaddr):
+    cap = mplane.model.Capability(label="ott-download", when = "now ... future / 10s")
+    cap.add_parameter("source.ip4",ipaddr)
+    cap.add_parameter("content.url")
+    cap.add_result_column("time")
+    for c in caplist:
+        cap.add_result_column(c)
+    return cap
+
+# return 1 if the result is an "offered" parameter (see capability)
+def contains_result(cap):
+    for c in caplist:
+        if cap.has_result_column(c):
+            return 1
+    return 0
+
+# this inherited class implements the OTT service based on the mplane scheduler
+# - gets parameters from probe-ott via _ott_process
+# - sets the result YAML
 class OttService(mplane.scheduler.Service):
-    """
-    This class parses the capabilities and executes the external
-    probe-ott software with the parameters specified in the
-    parameters
-
-    this inherited class implements the OTT service based on the mplane scheduler
-    - gets parameters from probe-ott via ott_process
-    - sets the result YAML
-
-    """
-
-    def __init__(self, ipaddr):
-        """
-        verify the capability is acceptable
-        and setting up the service
-
-        """
-
-        self.caplist = ["bandwidth.nominal.kbps", "http.code.max", "http.redirectcount.max", "qos.manifest", "qos.content", "qos.aggregate", "qos.level" ]
-        cap = self.assembleCapabilities(ipaddr)
-        if not ((cap.has_parameter("source.ip4")) and (cap.has_parameter("content.url")) and (self.contains_result(cap))):
+    def __init__(self, cap):
+        # verify the capability is acceptable
+        if not ((cap.has_parameter("source.ip4")) and
+                (cap.has_parameter("content.url")) and
+                (contains_result(cap))):
             raise ValueError("capability not acceptable")
         super(OttService, self).__init__(cap)
 
     def run(self, spec, check_interrupt):
-        """
-        get the parameters and execute the service
-        read the output JSON from probe-ott from STDOUT
-        create the result and send it back
-  
-        """
+        # unpack parameters
+      try:
+        period = spec.when().period().total_seconds()
 
-        try:
-          period = spec.when().period().total_seconds()
-          duration = spec.when().duration().total_seconds()
-          o_duration = duration
-          if spec.has_parameter("content.url"):
-              url = spec.get_parameter_value("content.url")
-          else:
-              raise ValueError("Missing URL")
+        if spec.has_parameter("content.url"):
+            url = spec.get_parameter_value("content.url")
+            ott_process = _ott_process(period, url)
+        else:
+            raise ValueError("Missing URL")
       
-          res = mplane.model.Result(specification=spec)
-          index = -1
-          s = datetime.utcnow()
-          while (duration > 0):
-            index += 1
-            o_process = self.ott_process(period, url)
-            duration -= period
-            jsonS = ""
-            for line in o_process.stdout:
-              strLine = line.decode()
-              if strLine is "}":
-                 jsonS += strLine
-                 break
-              jsonS += strLine
+        # read output from ping
+        jsonS = ""
+        for line in ott_process.stdout:
+            strLine = line.decode()
+            if strLine is "}":
+                jsonS += strLine
+                break
+            jsonS += strLine
 
-            jsonO = json.loads(jsonS)
+        jsonO = json.loads(jsonS)
+        e = str(datetime.now())
+        s = str(datetime.now()-timedelta(seconds=period))
 
-            #print(jsonS)
-            errcode = [jsonO["manifestQos.Max"], jsonO["contentQos.Max"]]
-            res.set_result_value("time", 				str(s + timedelta(seconds=(index*period) ) ), index)
-            res.set_result_value("bandwidth.nominal.kbps",	jsonO["nominalBitrate.Max"], index)
-            res.set_result_value("http.code.max", 		jsonO["httpCode.Max"], index)
-            res.set_result_value("http.redirectcount.max", 	jsonO["redirect.Max"], index)
-            res.set_result_value("qos.manifest", 			errcode[0], index)
-            res.set_result_value("qos.content", 			errcode[1], index)
-            res.set_result_value("qos.aggregate", 		min(errcode), index)
-            res.set_result_value("qos.level", 			jsonO["qualityIndex.Max"], index)
-          res.set_when(mplane.model.When(a=s, b=(  s + timedelta( seconds=((index+1)*period) ) )))
-          return res
-        except:
-          print("Unexpected error in run:", sys.exc_info())
-          raise
+        # derive a result from the specification
+        res = mplane.model.Result(specification=spec)
+        #print(jsonS)
 
+        # put actual start and end time into result
+        errcode = [jsonO["manifestQos.Max"], jsonO["contentQos.Max"]]
+        res.set_when(mplane.model.When( s, e))
+        res.set_result_value("time", s)
+        res.set_result_value("bandwidth.nominal.kbps", jsonO["nominalBitrate.Max"], 0)
+        res.set_result_value("http.code.max", jsonO["httpCode.Max"], 0)
+        res.set_result_value("http.redirectcount.max", jsonO["redirect.Max"], 0)
+        res.set_result_value("qos.manifest", errcode[0], 0)
+        res.set_result_value("qos.content", errcode[1], 0)
+        res.set_result_value("qos.aggregate", min(errcode), 0)
+        res.set_result_value("qos.level", jsonO["qualityIndex.Max"], 0)
+        print(res)
+        return res
+      except:
+        print("Unexpected error in run:", sys.exc_info())
+        raise
 
-    def assembleCapabilities(self, ipaddr):
-        """
-        method for assembling the offered parameters
-    
-        """
-
-        cap = mplane.model.Capability(label="ott-download", when = "now ... future / 1s")
-        cap.add_parameter("source.ip4",ipaddr)
-        cap.add_parameter("content.url")
-        cap.add_result_column("time")
-        for c in self.caplist:
-            cap.add_result_column(c)
-        return cap
-
-
-    def contains_result(self, cap):
-        """
-        returns True if the offered parameter is listed in the capabilities
-
-        """
-
-        for c in self.caplist:
-            if cap.has_result_column(c):
-                return True
-        return False
-
-    def ott_process(self, period=None, url=None):
-        """
-        This method starts the measurement and
-        opens a pipe to redirect the output
-        to STDOUT
-        
-        """
-
-        ott_argv = ["probe-ott"]
-        ott_argv += [ "--slot", "-1"]
-        if period is not None:
-            ott_argv += ["--mplane", str(int(period))]
-        if url is not None:
-            ott_argv += ["--url", str(url)]
-
-        print("running " + " ".join(ott_argv))
-
-        return subprocess.Popen(ott_argv, stdout=subprocess.PIPE)
 
 class OttProbe():
     """
     This class manages interactions with the supervisor:
     registration, specification retrievement, and return of results    
-
     """
 
     def __init__(self):
         """
         initiates a OTT probe for component-initiated workflow based on command-line arguments  
-
         """
-
         self.parse_args()
         headers={"content-type": "application/x-mplane+json"}
         if self.certfile is None:
+            if( self.forget_mplane_identity is not None ):
+                headers={"content-type": "application/x-mplane+json","Forget-MPlane-Identity": self.forget_mplane_identity}
             self.pool = HTTPConnectionPool(self.supervisorhost, self.supervisorport, headers=headers)
         else:
             self.pool = HTTPSConnectionPool(self.supervisorhost, self.supervisorport, key_file=self.key, cert_file=self.certfile, ca_certs=self.ca, headers=headers)
@@ -205,7 +177,7 @@ class OttProbe():
         self.scheduler = mplane.scheduler.Scheduler(self.certfile, self.certfile)
 
         if self.ip4addr is not None:
-            self.scheduler.add_service(OttService(self.ip4addr))
+            self.scheduler.add_service(OttService(capability(self.ip4addr)))
     
     def register_capabilities(self):
         print( "Registering capabilities to supervisor at " + self.supervisorhost + ":" + str(self.supervisorport) )
@@ -217,10 +189,14 @@ class OttProbe():
                 caps_list = caps_list + mplane.model.unparse_json(cap) + ","
         caps_list = "[" + caps_list[:-1].replace("\n","") + "]"
         
+        if self.forget_mplane_identity is None:
+           self.forget_mplane_identity = ""
+
         while True:
             try:
                 res = self.pool.urlopen('POST', "/" + REGISTRATION_PATH, 
-                     body=caps_list.encode("utf-8"))
+                    body=caps_list.encode("utf-8"), 
+                    headers={"content-type": "application/x-mplane+json", "Forget-MPlane-Identity": self.forget_mplane_identity})
                 
                 if res.status == 200:
                     body = json.loads(res.data.decode("utf-8"))
@@ -246,11 +222,10 @@ class OttProbe():
         Poll the supervisor for specifications
         
         """
-
         url = "/" + SPECIFICATION_PATH
         
         # send a request for specifications
-        res = self.pool.request('GET', url)
+        res = self.pool.request('GET', url, headers={"Forget-MPlane-Identity": self.forget_mplane_identity})
         if res.status == 200:
             
             # specs retrieved: split them if there is more than one
@@ -263,16 +238,24 @@ class OttProbe():
                 if isinstance(reply, mplane.model.Exception):
                     result_url = "/" + RESULT_PATH
                     # send result to the Supervisor
-                    res = self.pool.urlopen('POST', result_url,
-                            body=mplane.model.unparse_json(reply).encode("utf-8"))
+                    myheaders={}
+                    if self.forget_mplane_identity is None:
+                       myheaders={"content-type": "application/x-mplane+json"}
+                    else:
+                       myheaders={"content-type": "application/x-mplane+json", "Forget-MPlane-Identity": self.forget_mplane_identity}
+                    res = self.pool.urlopen('POST', result_url, 
+                            body=mplane.model.unparse_json(reply).encode("utf-8"), 
+                            headers=myheaders)
                     return
                 
+                # enqueue job
                 job = self.scheduler.job_for_message(reply)
                 
                 # launch a thread to monitor the status of the running measurement
                 t = threading.Thread(target=self.return_results, args=[job])
                 t.start()
                 
+        # not registered on supervisor, need to re-register
         elif res.status == 428:
             print("\nRe-registering capabilities on Supervisor")
             self.register_to_supervisor()
@@ -283,11 +266,11 @@ class OttProbe():
       Monitors a job, and as soon as it is complete sends it to the Supervisor
         
       """
-
       try:
         url = "/" + RESULT_PATH
         reply = job.get_reply()
         
+        # check if job is completed
         while job.finished() is not True:
             if job.failed():
               try:
@@ -300,10 +283,14 @@ class OttProbe():
         if isinstance (reply, mplane.model.Receipt):
             reply = job.get_reply()
         
-        res = self.pool.urlopen('POST', url, body=mplane.model.unparse_json(reply).encode("utf-8") ) 
+        # send result to the Supervisor
+        res = self.pool.urlopen('POST', url, 
+                body=mplane.model.unparse_json(reply).encode("utf-8") ) 
 
+        # handle response
         if res.status == 200:
-            print("Result for " + reply.get_label() + " successfully returned!")
+#            print("Result for " + reply.get_label() + " successfully returned!")
+            print("Result successfully returned!")
         else:
             print("Error returning Result for " + reply.get_label())
             print("Supervisor said: " + str(res.status) + " - " + res.data.decode("utf-8"))
@@ -314,19 +301,22 @@ class OttProbe():
     
     def parse_args(self):
         global args
-        parser = argparse.ArgumentParser(description="Run an mPlane OTT probe server")
-        parser.add_argument('--disable-ssl', action='store_true', default=False, dest='DISABLE_SSL', 
-								help='Disable secure communication')
-        parser.add_argument('-n', '--ip4addr', 	
-			metavar="source-v4-address", 		help="Ping from the given IPv4 address")
-        parser.add_argument('-c', '--certfile',
-	 		metavar="cert-file-location", 		help="Location of the configuration file for certificates")
-        parser.add_argument('-d', '--supervisorhost',
-		 	metavar="supervisorhost", 		help="IP or host name where supervisor runs (default: localhost)")
-        parser.add_argument('-p', '--supervisorport',
-			metavar="supervisorport", 		help="port on which supervisor listens (default: 8888)")
+        parser = argparse.ArgumentParser(description="Run an mPlane ping probe server")
+        parser.add_argument('--ip4addr', '-4', metavar="source-v4-address",
+                            help="Ping from the given IPv4 address")
+        parser.add_argument('--disable-ssl', action='store_true', default=False, dest='DISABLE_SSL',
+                            help='Disable secure communication')
+        parser.add_argument('--certfile', metavar="cert-file-location",
+                            help="Location of the configuration file for certificates")
+        parser.add_argument('--supervisorhost', metavar="supervisorhost",
+                            help="IP or host name where supervisor runs (default: localhost)")
+        parser.add_argument('--supervisorport', metavar="supervisorport",
+                            help="port on which supervisor listens (default: 8888)")
+        parser.add_argument('--forget-mplane-identity', metavar="forget_mplane_identity",
+                            help="ID to use in non-secure mode instead of certificate's subject")
         args = parser.parse_args()
         
+        self.forget_mplane_identity = args.forget_mplane_identity
         self.supervisorhost = args.supervisorhost or DEFAULT_SUPERVISOR_IP4
         self.supervisorport = args.supervisorport or DEFAULT_SUPERVISOR_PORT
         
@@ -363,18 +353,24 @@ class OttProbe():
 
 
 def manually_test_ott():
-    svc = OttService("127.0.0.1")
+    svc = OttService(capability("127.0.0.1"))
     spec = mplane.model.Specification(capability=svc.capability())
     spec.set_parameter_value("source.ip4", "127.0.0.1")
     spec.set_parameter_value("content.url", "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8")
-    spec.set_when("now + 20s / 10s")
+    spec.set_when("now + 10s / 10s")
 
     res = svc.run(spec, lambda: False)
     print(repr(res))
     print(mplane.model.unparse_yaml(res))
 
+# For right now, start a Tornado-based ping server
 if __name__ == "__main__":
+    if platform.system() != "Linux":
+        print("Linux is supported only. Output lines of ping command won't probably be parsed correctly.")
+        exit(2)
+    
     mplane.model.initialize_registry()
+    
     ottprobe = OttProbe()
 #    manually_test_ott() # uncomment this line for testing 
     ottprobe.register_capabilities()
